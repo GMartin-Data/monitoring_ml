ec005eb124e4bac107a9146f7025edd0201abd66
Greg <gregory.martin.data@gmail.com>
2025-01-22 11:10:09 +0100

    Fix issues with mapping labels

--- monitoring/app/model_utils.py
+++ monitoring/app/model_utils.py
@@ -1,11 +1,32 @@
+from logging import getLogger
+from pathlib import Path
+
 import joblib
 import pandas as pd
 
 
-def load_model(model_path: str = "model.joblib") -> "model":
-    """Utility function to load the model."""
-    model = joblib.load(model_path)
-    return model
+logger = getLogger(__name__)
+
+
+def load_model():
+    """
+    Load the trained model.
+
+    Uses the current file's location to determine the correct path to model.joblib,
+    ensuring it works regardless of where the application is started from
+    """
+    # Construct the absolute path to 'model.joblib'
+    current_dir = Path(__file__).parent.absolute()
+    model_path = current_dir / "model.joblib"
+
+    try:
+        logger.info(f"⏳ Attempting to load model from {model_path}")
+        model = joblib.load(model_path)
+        logger.info("✅ Model loaded successfully!")
+        return model
+    except Exception as e:
+        logger.error(f"❌ Failed to load model: {str(e)}")
+        raise
 
 
 def predict(model: "model", data: pd.DataFrame) -> str:
@@ -18,5 +39,10 @@ def predict(model: "model", data: pd.DataFrame) -> str:
     Returns:
         str: the penguin's specie.
     """
+    label_mapping = {
+        0: "Adelie",
+        1: "Chinstrap",
+        2: "Gentoo"
+    }
     y_pred = model.predict(data)
-    return y_pred
+    return label_mapping[y_pred[0]]
