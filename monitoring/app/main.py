804f12433468cdd383436c16d8a5038bbaa06615
Greg <gregory.martin.data@gmail.com>
2025-01-22 08:31:21 +0100

    First brew of main.py

--- monitoring/app/main.py
+++ monitoring/app/main.py
@@ -1,6 +1,7 @@
 from typing import Literal
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+import pandas as pd
 from pydantic import BaseModel, Field
 
 from model_utils import load_model, predict
@@ -25,3 +26,20 @@ class FeaturesInput(BaseModel):
 
 class PredictionOutput(BaseModel):
     species: SPECIES
+    
+
+model = load_model()
+
+
+@app.post(
+    "/predict",
+    response_model=PredictionOutput,
+    responses={
+        201: {"description": "prediction processed"}
+    })
+def give_prediction(features_input: FeaturesInput):
+    X_input_dict = features_input.model_dump()
+    X_input = pd.DataFrame(X_input_dict, index=[0])
+    y_pred = predict(model, X_input)
+    
+    return PredictionOutput(y_pred)
